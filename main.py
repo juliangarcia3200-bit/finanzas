62d460cf07dc45115d7f40e0caaf885273a7a41c
DarwinSanta82 <gerencia@inteligenciadedatos.com>
2025-08-04 18:42:02 -0500

    Funcion interactiva Interes Simple()

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from funciones_financieras import interes_simple
 
-monto = 10000000
-porcentaje = 0.095
-n = 3
+monto = float(input("Ingrese el monto Inicial "))
+porcentaje = float(input("Ingrese la tasa de interes "))
+n = int(input("Ingrese el numero de meses "))
 
 interes = interes_simple(monto, porcentaje, n)
 print(f"El intereses sobre el capital {monto:,.2f}, asciende a ${interes:,.2f} durante los {n} meses")
