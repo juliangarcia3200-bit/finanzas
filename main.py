61bbd26a10e395b2298899b75ca23b1bba80858c
juliangarcia3200 <juliangarcia3200@gmail.com>
2025-08-04 18:39:58 -0500

    Agrego version interactiva con input()

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from funciones_financieras import interes_compuesto
 
-capital = 1000.0  # Capital inicial
-tasa = 0.05  # Tasa de interés anual (5%)   
-tiempo = 5  # Tiempo en años
+capital = float(input("Ingrese el capital inicial:")) # Capital inicial
+tasa = float(input("Ingrese la tasa:"))  # Tasa de interés anual (5%)   
+tiempo = float(input("Ingrese el tiempo:"))  # Tiempo en años
 
 resultado = interes_compuesto(capital, tasa, tiempo)
 print(f"El monto total después de {tiempo} años es: {resultado:.2f}")
